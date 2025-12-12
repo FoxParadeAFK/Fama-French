227c5e465aab89c62cd50f01cec55211a3d8ab84
FoxParadeAFK <s.kittwong004@gmail.com>
2025-12-12 17:20:39 +0000

    Tested and current finalized dataset generator

---
+++ notebooks/cli/data.py
@@ -0,0 +1,49 @@
+import argparse
+import numpy as np
+import sys
+
+dependant: str = "y"
+
+def generator(m: int, n: int, seed: int, spread: int) -> tuple:
+  """
+  Generates a csv containing a number of points with a specific n
+  """ 
+
+  np.random.seed(seed)
+
+  slope: np.ndarray = np.random.uniform(-5, 5, n)
+  x: np.ndarray = np.random.uniform(-100, 100, m * n).reshape(-1, n)
+  intercept: float = np.random.uniform(-10, 10)
+  noise: np.ndarray = np.random.normal(0, spread, m)
+  y: np.ndarray = (x @ slope) + intercept + noise
+
+  independant: np.ndarray = np.array(range(n))
+  header: str = f"{dependant}, " + f"{', '.join(f"x_{x}" for x in independant)}"
+
+  return x, y, header
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+    prog = "Generates random points",
+    description = "Generates a CSV file of nD with normal noise",
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter
+  )
+
+  parser.add_argument('-fi', dest = 'file_name', type = str, default = "data.csv", help = 'name of the csv file')
+  parser.add_argument('-se', dest = 'seed', type = int, required = True, help = 'random seed')
+  parser.add_argument('-m', dest = 'm', type = int, default = 100, help = 'number of sample points')
+  parser.add_argument('-n', dest = 'n', type = int, default = 1, help = 'dimensions of the dataset')
+  parser.add_argument('-sp', dest = 'spread', type = int, default = 60, help = 'noise spread')
+
+  arguments: argparse.Namespace = parser.parse_args() 
+
+  try:
+    x, y, header = generator(m = arguments.m, n = arguments.n, seed = arguments.seed, spread = arguments.spread)
+    data: np.ndarray = np.column_stack((y, x))
+
+    np.savetxt(arguments.file_name, data, header = header, comments = "")
+    print(f"Successfully generated {len(data)} points in '{arguments.file_name}'")
+
+  except Exception as exception:
+    print(f"Error: {exception}")
+    sys.exit(1)
---
+++ notebooks/cli/tests/test_data.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pytest
+from data import generator
+
+@pytest.mark.parametrize("m, n", [
+  # (invalid, boundary, valid) i, b, v
+  (0, 1), # (b, b)
+  (4, 1), # (v, b)
+  (0, 2), # (b, v)
+  (10, 3), # (v, v) 
+  (200, 3), # (v, v)
+])
+def test_count_and_dimensions_valid(m: int, n: int): # generator requires parameters 'm' and 'n'. test data shape is within the parameters
+  seed: int = 45 # arbitrary seed number
+  spread: int = 60 # arbitrary spread number
+
+  x, y, _ = generator(m = m, n = n, seed = seed, spread = spread)
+  assert x.shape == (m , n)
+  assert y.shape == (m ,)
+
+@pytest.mark.parametrize("m, n", [
+  # (invalid, boundary, valid) i, b, v
+  (-1, 0), # (i, i)
+  (-10, -1), # (i, i)
+])
+def test_count_and_dimensions_invalid(m: int, n: int): # failure if the number of samples is negative or if the dimensions are below 2d
+  seed: int = 45 # arbitrary seed number
+  spread: int = 60 # arbitrary spread number
+
+  with pytest.raises(ValueError): generator(m = m, n = n, seed = seed, spread = spread)
+
+@pytest.mark.parametrize("seed", [
+  100,
+  200,
+  250,
+  275,
+])
+def test_seed_reproducibility(seed: int):
+  m: int = 10 # arbitrary seed number
+  n: int = 3 # arbitrary seed number
+  spread: int = 60 # arbitrary spread number
+
+  x_I, y_I, _ = generator(m = m, n = n, seed = seed, spread = spread)
+  x_II, y_II, _ = generator(m = m, n = n, seed = seed, spread = spread)
+
+  data_I: np.ndarray = np.column_stack((x_I, y_I))
+  data_II: np.ndarray = np.column_stack((x_II, y_II))
+
+  np.testing.assert_array_equal(data_I, data_II)
+  np.testing.assert_array_equal(data_II, data_I)
+
+
+@pytest.mark.parametrize("seed_I, seed_II", [
+  (100, 200),
+  (12, 34),
+  (47, 98),
+])
+def test_seed_reproducibility_(seed_I: int, seed_II):
+  m: int = 10 # arbitrary seed number
+  n: int = 3 # arbitrary seed number
+  spread: int = 60 # arbitrary spread number
+
+  x_I, y_I, _ = generator(m = m, n = n, seed = seed_I, spread = spread)
+  x_II, y_II, _ = generator(m = m, n = n, seed = seed_II, spread = spread)
+
+  data_I: np.ndarray = np.column_stack((x_I, y_I))
+  data_II: np.ndarray = np.column_stack((x_II, y_II))
+
+  with pytest.raises(AssertionError):
+    np.testing.assert_array_equal(data_I, data_II)
