9788de8e964c9f1445a07bee48bda805d70cf637
FoxParadeAFK <s.kittwong004@gmail.com>
2026-01-14 15:52:30 +0000

    Another path issue again

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -2,6 +2,16 @@ from typing import Any
 import httpx
 import pytest
 from fastapi.testclient import TestClient
+
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(parent_dir)
+
 from main import api
 
 @pytest.fixture(scope = "module")
